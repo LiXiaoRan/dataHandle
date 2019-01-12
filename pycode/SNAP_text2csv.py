86e09863be19bc0e8ebab236d1009cdf682f64d1
LiXiaoRan <997843911@qq.com>
2019-01-12 11:09:14 +0800

    增加了pagerank的计算代码，但是精度我还不知道，同时修改了数据处理部分的逻辑，目前的csv文件为了方便都不带headinfo

--- pycode/SNAP_text2csv.py
+++ pycode/SNAP_text2csv.py
@@ -1,30 +1,38 @@
 import csv
 
+#文件名
 FILENAME = 'gplus_combined.csv'
 
+#读取文件的文件夹路径
 FILEINNAME = '../dataset/'
+#结果文件的文件夹路径
 FILEOUTNAME = '../resultDataset/'
 
+# 读取文件的路径
 FILEINPATH = FILEINNAME + FILENAME
+# 结果文件的路径
 FILEOUTPATH = FILEOUTNAME + FILENAME
 
 csvFile = open(FILEINPATH, "r")
-csvoutFile = open(FILEOUTPATH, "w")
+csvoutFile = open(FILEOUTPATH, "w",newline='')
 
 reader = csv.reader(csvFile)
 writer = csv.writer(csvoutFile)
 
 list = []
+#读文件并且处理
 for item in reader:
     result = item[0].split(' ')
+    # print(result)
     list_item = {'source': result[0], 'target': result[1]}
     print(list_item)
     list.append(list_item)
 
 csvFile.close()
 
-head_info = ['source', 'target']
-writer.writerow(head_info)
+# 讲处理后的文件写入csv中
+# head_info = ['source', 'target']
+# writer.writerow(head_info)
 
 for item in list:
     w_row = [item['source'], item['target']]
