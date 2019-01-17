ca9a9d0de600f42bb176e4545a1e797076190b73
LiXiaoRan <997843911@qq.com>
2019-01-17 13:58:17 +0800

    完成了把pagerank和中心性合起来的代码

--- pycode/combine_data.py
+++ pycode/combine_data.py
@@ -1,7 +1,45 @@
 import csv
 
 _CSV = ".csv"
-FILE_PATH = "../resultDataset/"
-FILE_OUT_PATH = FILE_PATH + "filename" + "_cc" + _CSV
 
 
+def merge_data(FILE_NAME):
+    FILE_PATH = "../resultDataset/"
+    FILE_OUT_PATH = FILE_PATH + FILE_NAME + "_pg_cc" + _CSV
+
+    print("读取文件")
+    csvFilepg = open(FILE_PATH + FILE_NAME + "_pg" + _CSV, "r")
+    csvFilecc = open(FILE_PATH + FILE_NAME + "_cc" + _CSV, "r")
+    csvoutFile = open(FILE_OUT_PATH, "w", newline='')
+
+    reader_pg = csv.reader(csvFilepg)
+    reader_cc = csv.reader(csvFilecc)
+    writer = csv.writer(csvoutFile)
+
+    print("读取文件完毕")
+    nodeid_list = []
+    pgrank_list = []
+    cc_list = []
+
+    print("装载 " + FILE_PATH + FILE_NAME + "_pg" + _CSV + " 的数据")
+    for item in reader_pg:
+        nodeid_list.append(item[0])
+        pgrank_list.append(item[1])
+
+    print("装载 " + FILE_PATH + FILE_NAME + "_cc" + _CSV + " 的数据")
+    for item in reader_cc:
+        cc_list.append(item[1])
+
+
+    print("开始讲数据写入: "+FILE_OUT_PATH)
+    print("数据结构为；nodeid,pagerank,closeness_centrality")
+    for nodeid, pgrank, cc in zip(nodeid_list, pgrank_list, cc_list):
+        writer.writerow([nodeid, pgrank, cc])
+
+    print("写入完毕")
+    csvFilepg.close()
+    csvFilecc.close()
+    csvoutFile.close()
+
+
+merge_data("Cit_HepPh")
