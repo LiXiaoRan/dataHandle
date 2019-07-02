e1bfa2de433b536d1841f26d00a1b6d320255de4
LiXiaoRan <997843911@qq.com>
2019-07-02 20:54:23 +0800

    完成了QNoack布局质量指标评价的代码，已经计算了中小型网络，本算法个gephi中的对比算法

--- pycode/QualityMeasure.py
+++ pycode/QualityMeasure.py
@@ -1,5 +1,6 @@
 import csv
 import numpy as  np
+from tqdm import tqdm
 
 
 def calEuclideanDistance(pos1, pos2):
@@ -19,6 +20,7 @@ def quality(LAYOUT_FILE_NAME, GRAPH_FILE_NAME):
 
     layoutFile = open(FILE_PATH + LAYOUT_FILE_NAME + _CSV, "r")
     graphFile = open(FILE_GRAPH_PATH, "r")
+    recordFile = open(FILE_PATH + 'experiment_save.txt', 'a')
 
     layoutFileReader = csv.reader(layoutFile)
     graphFileReader = csv.reader(graphFile)
@@ -35,46 +37,73 @@ def quality(LAYOUT_FILE_NAME, GRAPH_FILE_NAME):
         # print(position)
         posList.append({'id': position[0], 'position': [float(position[1]), float(position[2])]})
 
-    print(posList)
+    # print(posList)
 
     # 读取边数据
     print('读取边数据')
     for link in graphFileReader:
         links.append({'source': link[0], 'target': link[1]})
 
-    print(links)
+    # print(links)
 
     posListLength = len(posList)
     linksLength = len(links)
-    posListLength2=np.square(posListLength)
+    posListLength2 = np.square(posListLength)
     countNum = 0  # 当前迭代次数,用来统计百分比
     # print(posListLength)
     # 开始计算节点之间的总距离
     print("开始计算节点之间的总距离")
+    pbar = tqdm(total=posListLength2)
     for node in posList:
         for item in posList:
             # if node['id'] != item['id']:  # 这里其实根本没必要判断，反正是0，并且判断会影响性能。
             sumNodeDistance += calEuclideanDistance(node['position'], item['position'])
-            print('Total sumNodeDistance percent: {:.2%}'.format(float(countNum) / float(posListLength2)))
-            countNum += 1
+            pbar.update(1)
+            # print('Total sumNodeDistance percent: {:.2%}'.format(float(countNum) / float(posListLength2)))
+            # countNum += 1
             # print("sumNodeDistance is ", sumNodeDistance)
 
+    pbar.close()
     countNum = 0
 
     # 开始计算节点之间的总距离
     print("开始计算边之间的总距离")
-    for link in links:
+    for link in tqdm(links):
         sourcePos = posList[next(index for (index, d) in enumerate(posList) if d["id"] == link['source'])]['position']
         targetPos = posList[next(index for (index, d) in enumerate(posList) if d["id"] == link['target'])]['position']
-
         sumLinkDistance += calEuclideanDistance(sourcePos, targetPos)
-        print('Total sumLinkDistance percent: {:.2%}'.format(float(countNum) / float(linksLength)))
+        # print('Total sumLinkDistance percent: {:.2%}'.format(float(countNum) / float(linksLength)))
         countNum += 1
         # print("sumLinkDistance is ", sumLinkDistance)
 
     # 计算布局质量
-    q = (sumLinkDistance / linksLength) / (sumNodeDistance / posListLength2)
-    print(LAYOUT_FILE_NAME + "布局质量为：", q)
+    q = 1 / ((sumLinkDistance / linksLength) / (sumNodeDistance / posListLength2))
+    # q = (sumLinkDistance / linksLength) / (sumNodeDistance / posListLength2)
+    txt = LAYOUT_FILE_NAME + "布局质量为：" + str(q)
+    recordFile.write('\n')
+    recordFile.write(txt)
+    recordFile.write('\n')
+    print(txt)
+    # print(LAYOUT_FILE_NAME + "布局质量为：", q)
 
 
-quality('3elt.csv_5000', '3elt')
+arguments = [
+
+    ['facebook_combined2.csv_5000', 'facebook_combined2']
+    , ['fe_4elt2.csv_5000', 'fe_4elt2'], ['twitter_combined.csv_5000', 'twitter_combined'],
+    ['web_Google.csv_500', 'web_Google'], ['web_Google.csv_5000', 'web_Google'],
+    ['web_NotreDame.csv_5000', 'web_NotreDame'], ['web_Stanford.csv_5000', 'web_Stanford'],
+    ['zhishi_baidu_relatedpages.csv_500', 'zhishi_baidu_relatedpages']
+    , ['zhishi_baidu_relatedpages.csv_5000', 'zhishi_baidu_relatedpages']]
+
+# ['Blogs.csv_5000', 'Blogs']
+# ['3elt.csv_500', '3elt'], ['1138_bus.csv_5000', '1138_bus'],['commanche_dual.csv_5000', 'commanche_dual'],['Email_EuAll.csv_500', 'Email_EuAll'], ['Email_EuAll.csv_5000', 'Email_EuAll'],
+# quality('1138_bus.csv_5000', '1138_bus')
+
+# ['Email_EuAll.csv_500', 'Email_EuAll'], ['Email_EuAll.csv_5000', 'Email_EuAll']
+# quality('Email_EuAll.csv_500', 'Email_EuAll')
+quality('Email_EuAll.csv_5000', 'Email_EuAll')
+# quality('Email_EuAll.csv_500', 'Email_EuAll')
+# for arr in arguments:
+#     print(arr[1]+" 数据正在被计算")
+#     quality(arr[0], arr[1])
